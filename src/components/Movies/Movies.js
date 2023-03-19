c16079d667e4f71d624d970f4d2cf2bdb740f8a4
Peter Sidorov <kripns06@gmail.com>
2023-03-19 23:53:49 +1200

    feat/implement layout for SearchForm

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,21 +1,11 @@
+import SearchForm from '../SearchForm/SearchForm';
 import './Movies.css';
 
 function Movies() {
   return(
     <section className='movies'>
-      <form className='search-form'>
-        <fieldset className='search-form__input-wrapper'>
-          <input className='search-form__input' type='search-form' placeholder='Фильм' />
-          <button className='search-form__submit-button' />
-        </fieldset>
-        <fieldset className='checkbox-wrapper'>
-          <div>
-            <div></div>
-            <input type='checkbox' />
-          </div>
-          <label className='checkbox__lable'>Короткометражки</label>
-        </fieldset>
-      </form>
+      <SearchForm />
+      
     </section>
   )
 }
