6a40fe4cf7fd736c93429d6393d3796b2ad4d71b
Peter Sidorov <kripns06@gmail.com>
2023-05-28 18:32:51 +1200

    fix/ change route for redirect in ProtectedAuthRoute

--- src/components/ProtectedAuthRout/ProtectedAuthRout.js
+++ src/components/ProtectedAuthRout/ProtectedAuthRout.js
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 
 function ProtectedAuthRoute(props) {
   const { isLoggedIn, children } = props;
-  return isLoggedIn ? <Navigate to={'/'} replace /> : children;
+  return isLoggedIn ? <Navigate to={'/movies'} replace /> : children;
 }
 
 export default ProtectedAuthRoute;
